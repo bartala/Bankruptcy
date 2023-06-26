2aa1aa05b68a44637059d5cce7c3be5fd37e29d3
bartala <bartala@gmail.com>
2023-06-26 14:20:17 +0300

    ubuntu commit2

--- biotools.R
+++ biotools.R
@@ -28,7 +28,7 @@ id <- '1Ld6ZedLnxSYO8jvq8ZIA9GcbQEvuB6Dd' # Google Drive file ID to citations
 tools_DB <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
 
 # create updated citation list
-df <- merge(df, tools_DB, by.x = 'tool_pmid', by.y ="meta.PMID", all.x = TRUE)
+df <- merge(df, tools_DB[c('meta.PMID', 'stat', 'meta.Published_On')], by.x = 'tool_pmid', by.y ="meta.PMID", all.x = TRUE)
 colnames(df)[colnames(df) == "meta.Published_On"] <- "tool_pubyear"
 
 
@@ -42,16 +42,16 @@ df<-df[df$relative_age>=0,]
 x_good <- sqldf("select relative_age, avg(num_citations) as avg from df where stat = 'Available' group by relative_age")
 x_bad <- sqldf("select relative_age, avg(num_citations) as avg from df where stat = 'Unavailable'  group by relative_age")
 
-
 ggplot(x_good, aes(x = relative_age, y = avg)) +
   geom_point(shape = 1, colour = "black", fill = "black", size = 2, stroke = 2) +
   geom_smooth(se = FALSE, formula = y ~ x, method = 'loess', size = 2) +
-  geom_vline(xintercept = 14.5, linetype = 2, size = 1, colour = "darkgray") +
+  geom_vline(xintercept = 18, linetype = 2, size = 1, colour = "darkgray") +
   ggtitle("Average citation time series of all biomedical tools papers") +
   ylab("Average tool citation") +
   xlab("Years relative to publication date") +
   theme_bw() +
   geom_smooth(data = x_bad, aes(x = relative_age, y =avg), color = "red",size = 2,  method = 'loess', se = FALSE,formula = y ~ x) +
-  geom_point(data = x_bad, aes(x = relative_age, y = avg), color = "red", size = 2, shape = 1, stroke = 2)
+  geom_point(data = x_bad, aes(x = relative_age, y = avg), color = "red", size = 2, shape = 1, stroke = 2) +
+  xlim(0, 19)  # Set the x-axis limits from 0 to 19
 
 
