910df1e82d1a127033308628dd1c842ddc693372
Alon Bartal <bartala@gmail.com>
2023-07-18 14:07:24 +0300

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -6,8 +6,8 @@ from textblob import TextBlob
 # 8) Average tweet sentiment (see data_analysis.R)
 # Get the sentiment score of a text vector
 
-def sentiment_tweets_1():
-  before = pd.read_csv(os.path.join(PTH,before.csv))
+def sentiment_analyzer(file):
+  x = pd.read_csv(os.path.join(PTH,file,'.csv'))
   analyzer = SentimentIntensityAnalyzer()
   
   sentiment_scores = []
@@ -15,18 +15,18 @@ def sentiment_tweets_1():
   sentiment_neg = []
   sentiment_neu = []
   
-  for text in before['text']:
+  for text in x['text']:
     scores = analyzer.polarity_scores(text)
     sentiment_scores.append( scores['compound'] )
     sentiment_neg.append( scores['neg'] )
     sentiment_pos.append( scores['pos'] )
     sentiment_neu.append( scores['neu'] )
   
-  before['sentiment'] = sentiment_scores
-  before['pos'] = sentiment_pos
-  before['neg'] = sentiment_neg
-  before['neu'] = sentiment_neu
-  pd.to_csv(os.path.join(PTH,'before.csv'))
+  x['sentiment'] = sentiment_scores
+  x['pos'] = sentiment_pos
+  x['neg'] = sentiment_neg
+  x['neu'] = sentiment_neu
+  x.to_csv(os.path.join(PTH,'before.csv'))
 
 
 def sentiment_tweets():
@@ -40,5 +40,5 @@ def sentiment_tweets():
     sentiment.append( x.sentiment[0] )
     subjectivity.append( x.sentiment[1] ) # The subjectivity is a float within the range [0.0, 1.0] where 0.0 is very objective and 1.0 is very subjective.
 
- pd.to_csv(os.path.join(PTH,'sentiment.csv'))
- pd.to_csv(os.path.join(PTH,'subjectivity'))
+ sentiment.to_csv(os.path.join(PTH,'sentiment.csv'))
+ subjectivity.to_csv(os.path.join(PTH,'subjectivity'))
