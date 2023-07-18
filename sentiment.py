f5e72a503204b19ddd19ef329505446c9f8eeab7
Alon Bartal <bartala@gmail.com>
2023-07-18 13:09:05 +0300

    Create sentiment.py

---
+++ sentiment.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+# 8) Average tweet sentiment
+# Get the sentiment score of a text vector
+
+before = pd.read_csv(os.path.join(PTH,before.csv))
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+
+sentiment_scores = []
+sentiment_pos = []
+sentiment_neg = []
+sentiment_neu = []
+
+for text in before['text']:
+  scores = analyzer.polarity_scores(text)
+  sentiment_scores.append( scores['compound'] )
+  sentiment_neg.append( scores['neg'] )
+  sentiment_pos.append( scores['pos'] )
+  sentiment_neu.append( scores['neu'] )
+
+before['sentiment'] = sentiment_scores
+before['pos'] = sentiment_pos
+before['neg'] = sentiment_neg
+before['neu'] = sentiment_neu
