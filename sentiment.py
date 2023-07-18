c9f7bb17c48c4caac5c93ba385f0cf1776cfe157
Alon Bartal <bartala@gmail.com>
2023-07-18 13:41:52 +0300

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
+import re
+from textblob import TextBlob
 
 # 8) Average tweet sentiment (see data_analysis.R)
 # Get the sentiment score of a text vector
@@ -24,3 +25,18 @@ before['sentiment'] = sentiment_scores
 before['pos'] = sentiment_pos
 before['neg'] = sentiment_neg
 before['neu'] = sentiment_neu
+
+
+def sentiment_tweets():
+  tweets_R = pd.read_csv(os.path.join(PTH,'tweets_R.csv'))
+  result = [re.sub(r'http\S+', '', x) for x in tweets_R]
+  sentiment = []
+  subjectivity = []
+  
+  for text in result:
+    x = TextBlob(text)
+    sentiment.append( x.sentiment[0] )
+    subjectivity.append( x.sentiment[1] ) # The subjectivity is a float within the range [0.0, 1.0] where 0.0 is very objective and 1.0 is very subjective.
+
+ pd.to_csv(os.path.join(PTH,'sentiment.csv'))
+ pd.to_csv(os.path.join(PTH,'subjectivity'))
